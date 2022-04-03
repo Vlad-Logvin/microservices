15896306f0dd1c621385098bf15fe7ca2a12456d
Uladzislau Lohvin <Uladzislau.Lohvin@ukg.com>
2022-04-03 20:59:52 +0300

    optimize imports

--- resource-service/service/src/main/java/com/epam/resource/configuration/ServiceDevConfiguration.java
+++ resource-service/service/src/main/java/com/epam/resource/configuration/ServiceDevConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootConfiguration
 @ComponentScan("com.epam.resource")
